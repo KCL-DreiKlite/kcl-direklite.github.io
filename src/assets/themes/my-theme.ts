b78f64cc485d44728f22567d9efafe1ce4463638
KCL <player124564615@gmail.com>
2024-08-08 22:37:52 +0800

    feat(theme): 反轉dark mode的surface

--- src/assets/themes/my-theme.ts
+++ src/assets/themes/my-theme.ts
@@ -3,19 +3,19 @@ import Lara from "@primevue/themes/lara";
 
 export const MyTheme = definePreset(Lara, {
     semantic: {
-        primary: {
-            50: '{orange.50}',
-            100: '{orange.100}',
-            200: '{orange.200}',
-            300: '{orange.300}',
-            400: '{orange.400}',
-            500: '{orange.500}',
-            600: '{orange.600}',
-            700: '{orange.700}',
-            800: '{orange.800}',
-            900: '{orange.900}',
-            950: '{orange.950}'
-        },
+        // primary: {
+        //     50: '{orange.50}',
+        //     100: '{orange.100}',
+        //     200: '{orange.200}',
+        //     300: '{orange.300}',
+        //     400: '{orange.400}',
+        //     500: '{orange.500}',
+        //     600: '{orange.600}',
+        //     700: '{orange.700}',
+        //     800: '{orange.800}',
+        //     900: '{orange.900}',
+        //     950: '{orange.950}'
+        // },
         colorScheme: {
             light: {
                 primary: {
@@ -47,10 +47,10 @@ export const MyTheme = definePreset(Lara, {
             },
             dark: {
                 primary: {
-                    color: '{orange.800}',
+                    color: '{orange.700}',
                     inverseColor: '{orange.950}',
-                    hoverColor: '{orange.100}',
-                    activeColor: '{orange.200}'
+                    hoverColor: '{orange.900}',
+                    activeColor: '{orange.800}'
                 },
                 highlight: {
                     background: 'rgba(250, 250, 250, .16)',
@@ -59,18 +59,18 @@ export const MyTheme = definePreset(Lara, {
                     focusColor: 'rgba(255,255,255,.87)'
                 },
                 surface: {
-                    0: '#ffffff',
-                    50: '{orange.50}',
-                    100: '{orange.100}',
-                    200: '{orange.200}',
-                    300: '{orange.300}',
-                    400: '{orange.400}',
-                    500: '{orange.500}',
-                    600: '{orange.600}',
-                    700: '{orange.700}',
-                    800: '{orange.800}',
-                    900: '{orange.900}',
-                    950: '{orange.950}'
+                    950: '#ffffff',
+                    900: '{orange.50}',
+                    800: '{orange.100}',
+                    700: '{orange.200}',
+                    600: '{orange.300}',
+                    500: '{orange.400}',
+                    400: '{orange.500}',
+                    300: '{orange.600}',
+                    200: '{orange.700}',
+                    100: '{orange.800}',
+                    50: '{orange.900}',
+                    0: '{orange.950}'
                 }
             }
         },
@@ -91,13 +91,13 @@ export const MyTheme = definePreset(Lara, {
                 },
                 dark: {
                     root: {
-                        background: '{surface.900}'
+                        background: '{surface.100}'
                     },
                 }
             },
             border: {
                 radius: '0'
             },
-        }
+        },
     }
 })
