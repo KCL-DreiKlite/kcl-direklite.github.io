590be6703c3bb1199f48f0c245eefb8fe8c1b72c
KCL <player124564615@gmail.com>
2024-08-08 21:16:26 +0800

    chore: 簡化import路徑

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import process from 'process'
+import * as path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV !== 'production' ? '/kcl-dreiklite.github.io/' : '/'
+  base: process.env.NODE_ENV !== 'production' ? '/kcl-dreiklite.github.io/' : '/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@img': path.resolve(__dirname, './src/assets/images'),
+      '@cp': path.resolve(__dirname, './src/components'),
+      '@views': path.resolve(__dirname, './src/views'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+    }
+  }
 })
