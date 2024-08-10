a5713e1d01ec3b1397feef34a32fe50641a16ee1
KCL <player124564615@gmail.com>
2024-08-10 11:49:24 +0800

    feat(home): 新增主頁內文，並套用 css

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,53 +1,42 @@
 <script setup lang="ts">
-import Button from "primevue/button";
+
 </script>
 
 <template>
-  <div>
-    <div class="text-4xl">首頁</div>
-    <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis vehicula gravida. In varius sollicitudin
-      urna et bibendum. Sed vel nunc faucibus, luctus ante vel, rhoncus libero. Ut odio tellus, porttitor mattis libero
-      nec, vulputate bibendum lacus. Donec fringilla interdum felis et suscipit. Lorem ipsum dolor sit amet, consectetur
-      adipiscing elit. Morbi id nisl eleifend, vulputate dui vestibulum, tempor mauris. Suspendisse ullamcorper, lorem a
-      mollis pellentesque, nibh orci malesuada turpis, quis sodales nisl metus facilisis nibh. Etiam imperdiet enim non
-      commodo pretium. Nullam sit amet egestas eros. Phasellus vestibulum ornare odio. Integer dignissim rutrum euismod.
-      Aenean ornare justo quis tellus interdum, ut dictum nibh pulvinar. Morbi tincidunt lectus eget neque molestie
-      fringilla. Etiam mi sem, porttitor ut tellus at, convallis iaculis nulla.
+  <div class="flex flex-col items-center p-2 w-full h-full">
+    <div class="article">
+      <div>
+        <div class="text-xl text-center font-bold italic pb-1">
+          Hallo! Ich bin Klite.
+        </div>
+        <div class="text-xl text-center italic font-bold">
+          Jetzt lerne ich Detusch und möchte ich nach Deutsch leben und arbeiten.
+        </div>
+      </div>
+      <div>
+        嗨，我是 Klite Kuo，畢業於國立台北科技大學資訊工程系。現任職於某科技公司軟體設計工程師和某餐飲集團兼職服務員（aka
+        端盤子 master）。目標是定居德國並在那邊工作，因此現在也有在學德文，目前先以考過 Goethe A1 為主。
+      </div>
+      <div>
+        這個網站主要是讓我自己學習寫網頁用的，順便當作一個便宜簡單的部落格（畢竟 GitHub Pages
+        不用錢），用來存放我自己的一些筆記或心得分享。程式碼可以點擊上方的 GitHub 按鈕，或按這裡查看。
+      </div>
+      <div>
+        如果您對我寫的文章沒有興趣的話也可以把這個網站當作一個簡易非正式的履歷。總之就是想到什麼寫什麼。
+      </div>
     </div>
-
-    <div>Etiam sed interdum ipsum. Nullam orci mauris, placerat pellentesque augue et, cursus interdum leo. Fusce
-      elementum augue a sapien vulputate, vitae pretium turpis malesuada. Aliquam vulputate libero nunc, ut laoreet
-      risus rutrum quis. Donec sed vulputate nisi. Nullam dignissim est vitae eros sagittis, in tristique ex tincidunt.
-      Maecenas lacinia ornare viverra. Maecenas mollis, lorem et pretium lobortis, velit massa semper diam, vitae
-      egestas ligula arcu nec est. Donec eu ante molestie, volutpat nisl nec, faucibus turpis. Aliquam pulvinar dui ac
-      lectus aliquet, non laoreet elit tincidunt.
-    </div>
-
-    <div>Nam quis rhoncus eros. Suspendisse finibus vestibulum nulla, eu pretium nisl. Nulla hendrerit ornare ipsum
-      tristique dictum. Cras accumsan iaculis maximus. Morbi vehicula mauris at ipsum fermentum pulvinar ut non leo.
-      Nunc at egestas velit. Quisque vehicula aliquet nulla nec volutpat. Quisque gravida tincidunt dui eget sagittis.
-    </div>
-
-    <div>Sed vestibulum, est eu tristique ornare, sapien lectus tempor lacus, non congue ipsum felis a tortor.
-      Suspendisse eleifend diam id justo egestas rutrum. Suspendisse tincidunt est id felis ornare, id volutpat libero
-      pellentesque. In interdum diam ut purus eleifend malesuada. Praesent congue gravida libero nec vehicula. Cras
-      condimentum odio neque, non rutrum sem viverra a. Fusce aliquam dui condimentum lobortis fermentum. Morbi non
-      turpis non dui porttitor volutpat ac quis ex. Curabitur efficitur at tellus ut bibendum. Vivamus accumsan
-      tincidunt hendrerit.
-    </div>
-
-    <div>Proin ut malesuada odio, ac interdum erat. Quisque aliquam, eros at tincidunt ornare, lorem justo tristique
-      diam, laoreet dapibus dolor enim in augue. Nunc eget quam vitae lorem hendrerit pharetra. Nulla eget metus
-      euismod, commodo orci vitae, facilisis odio. Maecenas feugiat sapien pellentesque rutrum euismod. Praesent
-      imperdiet, enim sed ullamcorper tristique, augue mauris venenatis orci, sit amet hendrerit justo diam vel velit.
-      Integer in felis laoreet, semper justo vel, sodales lacus. Integer id urna ut mauris vulputate egestas.
-    </div>
-    <router-link to="/about">
-      <Button>hehe</Button>
-    </router-link>
   </div>
 </template>
 
 <style scoped>
+.article {
+  display: flex;
+  flex-direction: column;
 
+  gap: 1rem;
+  background: rgb(255, 165, 0, 15%);
+  width: 90vw;
+  border-radius: 1rem;
+  padding: 1.5rem;
+}
 </style>
